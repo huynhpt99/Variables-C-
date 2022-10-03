8c60197820f41dba74d22332ec35e5a78b053d80
huynhpt99 <80614276+huynhpt99@users.noreply.github.com>
2022-10-03 15:26:05 -0700

    Add files via upload

---
+++ variables.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+void main() {
+    int x;
+    x = 12;
+    int y = 321;
+
+    int age = 23;
+    float gpa = 3.58;
+    char grade = 'C';
+    char name[] = "Lip";
+
+    printf("My name is %s\n", name);
+    printf("I'm %d\n", age);
+    printf("My grade is %c\n", grade);
+    printf("And my gpa is %f\n", gpa);
+    return 0;
+}
+
